e9574fe528deba349c66d231a352b4fbf2aff349
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2022-03-16 17:55:42 +0100

    [TASK] Add license header

--- Tests/Unit/Validation/Validator/PhoneValidatorTest.php
+++ Tests/Unit/Validation/Validator/PhoneValidatorTest.php
@@ -2,6 +2,21 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * (c) Simon Schaufelberger
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
 namespace GertKaaeHansen\TYPO3Phone\Tests\Unit\Validation\Validator;
 
 use Nimut\TestingFramework\MockObject\AccessibleMockObjectInterface;
