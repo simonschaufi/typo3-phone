26eccf0c1a7876e17119e08ecf3105039d29e0fb
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2026-08-11 17:02:39 +0200

    [FEATURE] Compatibility with TYPO3 14

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,8 +13,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '4.1.0',
     'constraints' => [
         'depends' => [
-            'php' => '8.2.0-8.4.99',
-            'typo3' => '13.4.0-13.4.99',
+            'php' => '8.2.0-8.5.99',
+            'typo3' => '14.3.0-14.3.99',
         ],
         'conflicts' => [],
         'suggests' => [],
